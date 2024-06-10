3de74b27f02140e8cfa32d1b943d0e846fbd7916
0xturboblitz <florent.tavernier@gmail.com>
2024-06-10 11:12:54 +0200

    change next rewrites config

--- next.config.js
+++ next.config.js
@@ -1,21 +1,41 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  rewrites() {
-    return {
+    async rewrites() {
+      return {
         beforeFiles: [
-            {
-                source: '/:path*',
-                has: [
-                    {
-                        type: 'host',
-                        value: 'map.proofofpassport.com',
-                    },
-                ],
-                destination: '/map/:path*',
-            },
-        ]
-    }
-  }
-}
-
-module.exports = nextConfig
+          {
+            source: '/:path*',
+            has: [
+              {
+                type: 'host',
+                value: 'map.proofofpassport.com',
+              },
+            ],
+            destination: '/map/:path*',
+          },
+          {
+            source: '/map/_next/:path*',
+            has: [
+              {
+                type: 'host',
+                value: 'map.proofofpassport.com',
+              },
+            ],
+            destination: '/_next/:path*',
+          },
+          {
+            source: '/map/static/:path*',
+            has: [
+              {
+                type: 'host',
+                value: 'map.proofofpassport.com',
+              },
+            ],
+            destination: '/static/:path*',
+          },
+        ],
+      };
+    },
+  };
+  
+  module.exports = nextConfig;
