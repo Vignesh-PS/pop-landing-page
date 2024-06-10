c906259b90345667b8ea95820de30b84c75e748e
0xturboblitz <florent.tavernier@gmail.com>
2024-06-10 11:27:38 +0200

    other config

--- next.config.js
+++ next.config.js
@@ -34,6 +34,24 @@ const nextConfig = {
             destination: '/static/:path*',
           },
         ],
+        afterFiles: [
+            {
+                source: '/map/_next/:path*',
+                destination: '/_next/:path*',
+            },
+            {
+                source: '/map/static/:path*',
+                destination: '/static/:path*',
+            },
+            {
+                source: '/map/data/:path*',
+                destination: '/data/:path*',
+            },
+            {
+                source: '/map/favicon.ico',
+                destination: '/favicon.ico',
+            },
+        ],
       };
     },
     async headers() {
