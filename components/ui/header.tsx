a0d01cedbb30ad00c8580f3cb65711525b065bb0
0xturboblitz <florent.tavernier@gmail.com>
2024-06-15 09:17:24 -0700

    add map button on landing

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -48,6 +48,9 @@ export default function Header() {
                 <Link href="/signin" className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">About us</Link>
               </li> */}
               <li>
+                <Link href="https://map.proofofpassport.com/" rel="noopener noreferrer" className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3">
+                  <span>Map</span>
+                </Link>
                 <Link href="https://testflight.apple.com/join/WfZnZWfn" target="_blank" rel="noopener noreferrer" className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3">
                   <span>App Store</span>
                   <svg className="w-3 h-3 fill-current text-gray-400 shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
